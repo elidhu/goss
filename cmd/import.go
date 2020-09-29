71441c2ca6c80b32317377bbacc67b6c6612dd7e
Kevin Glasson <kevinglasson@gmail.com>
2020-09-29 23:15:40 +0800

    Enable the additional parsers to support multiple import formats.
    The underlying `koanf` library supports json, toml and yaml in addition
    to dotenv. The goss import command now supports these other formats
    through the use of the `--format flag`.
    
    ```bash
    goss import -f test.toml -t SecureString -p /dev --format toml
    ```

--- cmd/import.go
+++ cmd/import.go
@@ -9,9 +9,16 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
 	kdotenv "github.com/knadh/koanf/parsers/dotenv"
+	kjson "github.com/knadh/koanf/parsers/json"
+	ktoml "github.com/knadh/koanf/parsers/toml"
+	kyaml "github.com/knadh/koanf/parsers/yaml"
 	kfile "github.com/knadh/koanf/providers/file"
 	"github.com/spf13/cobra"
 )
+
+type unmarshaller interface {
+	Unmarshal(b []byte) (map[string]interface{}, error)
+}
 
 var (
 	// For flags
@@ -19,13 +26,31 @@ var (
 	importType   string
 	importPath   string
 	importOWrite bool
+	format       string
 
 	// importCmd represents the import command
 	importCmd = &cobra.Command{
 		Use:   "import",
 		Short: "Import parameters from a file",
 		Run: func(cmd *cobra.Command, args []string) {
-			err := importParameters(file, importType, importPath, importOWrite)
+
+			// Define the map of string to parsers
+			parsers := map[string]unmarshaller{
+				"json":   kjson.Parser(),
+				"toml":   ktoml.Parser(),
+				"yaml":   kyaml.Parser(),
+				"dotenv": kdotenv.Parser(),
+			}
+
+			// Check the format arg is for a supported format
+			err := isValidFormat(format)
+			if err != nil {
+				utils.PrintErrorAndExit(err)
+			}
+
+			parser := parsers[format]
+
+			err = importParameters(file, importType, importPath, importOWrite, parser)
 			if err != nil {
 				utils.PrintErrorAndExit(err)
 			}
@@ -37,25 +62,32 @@ func init() {
 	rootCmd.AddCommand(importCmd)
 
 	importCmd.Flags().StringVarP(
-		&file, "file", "f", "", "path to the file",
+		&file, "file", "f", "", "path to the file to import",
 	)
 	importCmd.MarkFlagRequired("file")
+
 	importCmd.Flags().StringVarP(
-		&importType, "type", "t", "", "parameter type",
-	)
-	importCmd.Flags().StringVarP(
-		&importPath, "path", "p", "", "parameter path",
+		&importType, "type", "t", "", "aws parameter type to use when importing",
 	)
 	importCmd.MarkFlagRequired("type")
+
+	importCmd.Flags().StringVarP(
+		&importPath, "path", "p", "", "base path to import the parameters to",
+	)
+	importCmd.MarkFlagRequired("path")
+
 	importCmd.Flags().BoolVarP(
-		&importOWrite, "overwrite", "o", false, "overwrite parameters if they exist",
+		&importOWrite, "overwrite", "o", false, "overwrite parameters if they exist, i.e. update them",
+	)
+
+	importCmd.Flags().StringVarP(
+		&format, "format", "", "dotenv", "file format. [json toml yaml dotenv]",
 	)
 }
 
-func importParameters(file string, typ string, path string, overwrite bool) error {
+func importParameters(file string, typ string, path string, overwrite bool, parser unmarshaller) error {
 
 	provider := kfile.Provider(file)
-	parser := kdotenv.Parser()
 
 	// Get the raw bytest from the provider
 	b, err := provider.ReadBytes()
@@ -102,4 +134,18 @@ func importParameters(file string, typ string, path string, overwrite bool) erro
 	fmt.Printf("Successfully imported %d / %d parameters", count, len(mp))
 
 	return nil
+}
+
+func isValidFormat(format string) error {
+	// Supported formats
+	supported := []string{"json", "toml", "yaml", "dotenv"}
+
+	for _, v := range supported {
+		if v == format {
+			return nil
+		}
+	}
+	return fmt.Errorf(
+		"Unsupported input file format `%s`. Supported formats are: %v", format, supported,
+	)
 }
